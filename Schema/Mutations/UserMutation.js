af2f15643f1b1d39b3c8f8b1aa68c1023fbc9a2f
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-05-11 18:39:24 +0500

    prettier

--- Schema/Mutations/UserMutation.js
+++ Schema/Mutations/UserMutation.js
@@ -3,18 +3,18 @@ const { Users } = require("../../models");
 const UserType = require("../TypeDefs/UserType");
 
 module.exports = {
- addUser: {
-  type: UserType,
-  args: {
-   name: { type: GraphQLString },
-   email: { type: GraphQLString },
-   gender: { type: GraphQLString },
-   status: { type: GraphQLString },
-   phone: { type: GraphQLString },
+  addUser: {
+    type: UserType,
+    args: {
+      name: { type: GraphQLString },
+      email: { type: GraphQLString },
+      gender: { type: GraphQLString },
+      status: { type: GraphQLString },
+      phone: { type: GraphQLString },
+    },
+    resolve: async (parent, args) => {
+      await Users.create(args);
+      return args;
+    },
   },
-  resolve: async (parent, args) => {
-   await Users.create(args);
-   return args;
-  },
- },
 };
--- Schema/TypeDefs/UserType.js
+++ Schema/TypeDefs/UserType.js
@@ -1,14 +1,14 @@
 const { GraphQLObjectType, GraphQLInt, GraphQLString } = require("graphql");
 const tableUser = "users";
 const UserType = new GraphQLObjectType({
- name: tableUser,
- fields: () => ({
-  id: { type: GraphQLInt },
-  name: { type: GraphQLString },
-  email: { type: GraphQLString },
-  gender: { type: GraphQLString },
-  status: { type: GraphQLString },
-  phone: { type: GraphQLString },
- }),
+  name: tableUser,
+  fields: () => ({
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    gender: { type: GraphQLString },
+    status: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
 });
 module.exports = UserType;
