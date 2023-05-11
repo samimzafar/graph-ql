41f36aa14a816683bfa0d926ba172338081c4a5e
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-05-11 18:39:08 +0500

    mutation and created user

--- Schema/TypeDefs/UserType.js
+++ Schema/TypeDefs/UserType.js
@@ -1,16 +1,14 @@
 const { GraphQLObjectType, GraphQLInt, GraphQLString } = require("graphql");
 const tableUser = "users";
 const UserType = new GraphQLObjectType({
-  name: tableUser,
-  fields: () => ({
-    id: { type: GraphQLInt },
-    name: { type: GraphQLString },
-    email: { type: GraphQLString },
-    gender: { type: GraphQLString },
-    status: { type: GraphQLString },
-    phone: { type: GraphQLString },
-    createdAt: { type: GraphQLInt },
-    updatedAt: { type: GraphQLInt },
-  }),
+ name: tableUser,
+ fields: () => ({
+  id: { type: GraphQLInt },
+  name: { type: GraphQLString },
+  email: { type: GraphQLString },
+  gender: { type: GraphQLString },
+  status: { type: GraphQLString },
+  phone: { type: GraphQLString },
+ }),
 });
 module.exports = UserType;
--- Schema/index.js
+++ Schema/index.js
@@ -1,12 +1,18 @@
 const graphql = require("graphql");
 const query = require("./Queries/UserQuery");
+const mutation = require("./Mutations/userMutation");
 const { GraphQLObjectType, GraphQLSchema } = graphql;
-
 const RootQuery = new GraphQLObjectType({
-  name: "GraphQL",
-  fields: {
-    usersRecord: query.getAllUsers,
-  },
+ name: "query",
+ fields: {
+  usersRecord: query.getAllUsers,
+ },
 });
 
-module.exports = new GraphQLSchema({ query: RootQuery });
+const Mutation = new GraphQLObjectType({
+ name: "mutation",
+ fields: {
+  createUser: mutation.addUser
+ }
+});
+module.exports = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
