9afdd421bec74b86e07b35b02500bbef5436e4e4
Camim Khan <47691227+samimzafar@users.noreply.github.com>
2023-05-31 20:39:46 +0500

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
- env: {
-  node: true,
-  commonjs: true,
-  es6: true,
- },
- extends: "eslint:recommended",
- globals: {
-  Atomics: "readonly",
-  SharedArrayBuffer: "readonly",
- },
- parserOptions: {
-  ecmaVersion: 2018,
- },
- rules: {},
- ignorePatterns: ["client/", "database/seeds/"],
+  env: {
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {},
+  ignorePatterns: ["client/", "database/seeds/"],
 };
